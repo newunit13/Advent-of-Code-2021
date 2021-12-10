df5c8eab542e33154e0f5ec17a52d27308357e7d
newunit13 <newunit13@gmail.com>
2021-12-09 19:31:25 -0500

    Day 7 - part 2

--- Day 7/solution.py
+++ Day 7/solution.py
@@ -1,4 +1,5 @@
 import numpy as np
+from collections import defaultdict
 
 with open('Day 7/test.txt') as f:
     data = f.read()
@@ -15,3 +16,18 @@ median = np.median(crab_ships)
 
 print(f'Part 1: {sum([abs(ship_pos - median) for ship_pos in crab_ships])}')
 
+def calculate_distance(ship_pos: int, target_pos: int) -> int:
+    return abs(ship_pos - target_pos)
+
+def calculate_fuel_needed(spaces_away: int) -> int:
+    return spaces_away * (spaces_away + 1) // 2
+
+ship_hash = defaultdict(int)
+
+for target in range(min(crab_ships), max(crab_ships) + 1):
+    for ship in crab_ships:
+        distance = calculate_distance(ship, target)
+        fuel_needed = calculate_fuel_needed(distance)
+        ship_hash[target] += fuel_needed
+
+print(f'Part 2: {min(ship_hash.values())}')
